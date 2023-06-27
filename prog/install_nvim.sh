7c9e67b73bb2bdcf736fb0bc5be25d9730488745
Luã Bida Vacaro <luabidaa@gmail.com>
2023-06-27 13:33:17 -0300

    Delete prog directory

--- prog/install_nvim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -P /tmp
-
-tar xzvf /tmp/nvim-linux64.tar.gz -C /tmp
-mv /tmp/nvim-linux64/bin/nvim /bin
-
